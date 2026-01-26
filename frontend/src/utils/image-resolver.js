61afa9617adceeb30bc64b6af0b1897e1588dfd7
Ehsan Qadir <ehsan.qadir@vigorant.com>
2026-01-27 03:54:42 +0500

    update imageResolver to construct full image path from base URL

--- frontend/src/utils/image-resolver.js
+++ frontend/src/utils/image-resolver.js
@@ -1,5 +1,11 @@
 export const imageResolver = (source) => {
-  const path = "" + source?.data?.attributes?.url;
+  const basePath = "https://akku4ojhingudehn.public.blob.vercel-storage.com/media/uploads";
+  const url = source?.data?.attributes?.url || "";
+  
+  // Extract just the filename/path by getting everything after the last slash
+  const relativePath = url.split('/').pop();
+  
+  const path = `${basePath}/${relativePath}`;
   const width = source?.data?.attributes?.width;
   const height = source?.data?.attributes?.height;
   const alt = source?.data?.attributes?.alternativeText;
