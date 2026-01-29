071108ce4bb92fcd5ea20daed643a500a83558b5
Ehsan Qadir <ehsan.qadir@vigorant.com>
2026-01-30 00:51:35 +0500

    add runtime specification for email sending API

--- api/mail/src/app/api/send-email.js
+++ api/mail/src/app/api/send-email.js
@@ -1,9 +1,11 @@
 import { Resend } from 'resend';
 
+export const runtime = 'nodejs';
+
 const resend = new Resend(process.env.RESEND_TOKEN);
 
 const headers = {
-  'Access-Control-Allow-Origin': '*', // lock later
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
